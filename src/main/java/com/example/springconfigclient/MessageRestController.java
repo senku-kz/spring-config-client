ea4761d675920740d65ba2fa7673584767eb34c1
Rustem <senku.kz@gmail.com>
2024-07-02 15:09:45 +0500

    first commit

---
+++ src/main/java/com/example/springconfigclient/MessageRestController.java
@@ -0,0 +1,18 @@
+package com.example.springconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageRestController {
+    @Value("${msg:Config Server is not working. Please check...}")
+    private String msg;
+
+    @GetMapping("/msg")
+    public String getMsg() {
+        return this.msg;
+    }
+}
